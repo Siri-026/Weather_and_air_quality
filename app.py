d02deb7a385ebecb8653438121f130963393cf06
Siri-026 <sirihegde05@gmail.com>
2025-02-23 13:06:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, session, request, redirect, url_for, jsonify,flash
 import requests
 from flask_sqlalchemy import SQLAlchemy
-from forms import CarbonFootPrintForm
+from flask-wtf import CarbonFootPrintForm
 import joblib
 import pandas as pd
 import plotly.express as px 
